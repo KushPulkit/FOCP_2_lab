df3bd189db6d66a6ac69e2b8319ce530e0b5cace
KushPulkit <24csu166@ncuindia.edu>
2025-01-30 21:29:20 +0530

    Create ques1.cpp

---
+++ ques1.cpp
@@ -0,0 +1,11 @@
+//Q1. WAP that reads user's name and age:
+#include<iostream>
+using namespace std;
+int main(){
+    char name[15];int age;
+    cout<<"What is your name? ";
+    cin>>name;
+    cout<<"What is your age? ";
+    cin>>age;
+    cout<<"Hello "<<name<<", you are "<<age<<" years old!";
+}
