8ecb7cca0518a26a49234887d0b3c02f385c87a4
KushPulkit <24csu166@ncuindia.edu>
2025-01-30 21:36:34 +0530

    Create ques10.cpp

---
+++ ques10.cpp
@@ -0,0 +1,13 @@
+/*Q10. WAP to enter two numbers and check if first is multiple of second using
+relational and logical operators*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    if(b!=0 && a%b==0)
+    cout<<a<<" is a multiple of "<<b;
+    return 0;
+}
