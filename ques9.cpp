f301c0f9be6a246bca91fd4a19fe317939244d5a
KushPulkit <24csu166@ncuindia.edu>
2025-01-30 21:35:32 +0530

    Create ques9.cpp

---
+++ ques9.cpp
@@ -0,0 +1,11 @@
+//Q9. Create a program that takes three inputs: char,int,float and display in formated table
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+char ch; int in; float f;
+cout<<"Enter character, integer and float values: ";
+cin>>ch>>in>>f;
+cout<<"Char\tInt\tFloat\n"<<ch<<"\t"<<in<<"\t"<<f;
+return 0;
+}
