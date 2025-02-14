21b765f965390e829fab2af1d7beeec23518150d
Pulkit Kush <24csu166@ncuindia.edu>
2025-02-14 13:05:34 +0530

    Create ques15.cpp

---
+++ Practice Assignment 1/ques15.cpp
@@ -0,0 +1,10 @@
+// Q15. Write a program to evaluate (a^b + c&d), where a,b,c,d are user inputs
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d;
+cout<<"Enter 4 inputs: ";
+cin>>a>>b>>c>>d;
+cout<<(a^b) + (c&d);
+return 0;
+}
