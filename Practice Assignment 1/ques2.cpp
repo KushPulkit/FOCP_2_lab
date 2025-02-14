5f63404b454ddcd5891a0218931e28544623a662
Pulkit Kush <24csu166@ncuindia.edu>
2025-02-14 12:59:49 +0530

    Create ques2.cpp

---
+++ Practice Assignment 1/ques2.cpp
@@ -0,0 +1,9 @@
+// Q2. Write a program to take two integers as inoout and display their sum
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter two numbers: ";
+    int a,b;
+    cin>>a>>b;
+    cout<<"Sum is "<<a+b;
+}
