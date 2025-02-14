3195d93631818a185cea322d7fafec2da28d087d
Pulkit Kush <24csu166@ncuindia.edu>
2025-02-14 13:01:28 +0530

    Create ques6.cpp

---
+++ Practice Assignment 1/ques6.cpp
@@ -0,0 +1,10 @@
+// Q6. WAP that calculates the area and perimeter of a rectangle using l and b
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+float l,b;
+cout<<"Enter Length and Width: ";
+cin>>l>>b;
+cout<<"Area: "<<l*b<<"\nPerimeter: "<<2*(l+b);
+}
